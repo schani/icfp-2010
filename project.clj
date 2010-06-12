dd1f48c75029bf52317d4e75468bd9011b244a8f
Mark Probst <mark.probst@gmail.com>
2010-06-12 15:33:06 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject icfp-2010 "0.0.1-SNAPSHOT"
+  :description "ICFP Contest 2010 - Funktion im Kopf der Mensch"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+		 [clj-simple-dist "0.0.1-SNAPSHOT"]]
+  :source-path "src/clj"
+  :java-source-path "src/java"
+  :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]
+		     [swank-clojure "1.2.1"]])
