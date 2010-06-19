74cd27f9835e49a377197dc7b6c21ca6fefd5e84
Martin Biely <git@biely.eu>
2010-06-19 19:37:51 +0200

    output for fuels

---
+++ src/clj/at/ac/tuwien/tilab/icfp2010/fuel.clj
@@ -0,0 +1,21 @@
+(ns at.ac.tuwien.tilab.icfp2010.fuel
+  (:use clojure.contrib.generic.math-functions)
+  (:use at.ac.tuwien.tilab.icfp2010.ternary)
+  )
+
+;; warning only up to 41 is supported
+
+(defn thing-to-string [thing]
+  (if (list? thing)
+    (apply str 
+	   (prefixes (count thing)) 
+	   (map thing-to-string thing)  
+	   )
+    (if (vector? thing)
+      (apply (map thing-to-string) thing)
+      (number-to-ternary thing))))
+
+
+(defn output-fuel "(((1))) is a fuel" 
+  [fuel]
+  (print (thing-to-string fuel)))
