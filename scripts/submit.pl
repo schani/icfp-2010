4a364ad9b557a9d4faa6213f53e6e4c1c10fdc5c
Edgar Holleis <edgar@ti16.(none)>
2010-06-20 15:58:08 +0200

    -little bugfixes

--- scripts/submit.pl
+++ scripts/submit.pl
@@ -6,8 +6,9 @@ require HTTP::Cookies;
 require HTML::Form;
 
 # Check command line
-my $mode = shift;
-die "Synopsis: ./submit.pl car | fuel | getcars | badcars" unless $mode eq "fuel" || $mode eq "car" || $mode eq "getcars" || $mode eq "badcars";
+my $synopsis = "Synopsis: ./submit.pl car | fuel | getcars | badcars";
+my $mode = shift || die $synopsis;
+die $synopsis unless $mode eq "fuel" || $mode eq "car" || $mode eq "getcars" || $mode eq "badcars";
 
 # Instanciate user agent, check for cookie, login if necessary
 my $ua = LWP::UserAgent->new;
@@ -200,9 +201,9 @@ if ($mode eq "car") {
 		}
 
 		if ($response->content =~ m/Car:<\/label>([0-3]+)<\/div>/) {
-			printf("id=%d car=%s\n", $carid, $1);
+			printf("%d %s\n", $carid, $1);
 		} else {
-			printf("id=%d parse error\n", $carid);
+			print STDERR sprintf("%d parse error\n", $carid);
 		}
 	}
 }
