f5753307297fadbaaa9767669ec14439e1f97f99
Mark Probst <mark.probst@gmail.com>
2010-06-20 15:32:22 +0200

    Distribution for genetic algorithm.

--- src/clj/at/ac/tuwien/tilab/icfp2010/genetic.clj
+++ src/clj/at/ac/tuwien/tilab/icfp2010/genetic.clj
@@ -1,6 +1,9 @@
 (ns at.ac.tuwien.tilab.icfp2010.genetic
   (:use at.ac.tuwien.tilab.icfp2010.cars
-	at.ac.tuwien.tilab.icfp2010.ternary)
+	at.ac.tuwien.tilab.icfp2010.ternary
+	at.ac.tuwien.tilab.icfp2010.search
+	at.ac.tuwien.complang.distributor.vsc)
+  (:require clojure.contrib.string)
   (:import [at.ac.tuwien.tilab.icfp2010 Fuel]))
 
 (defn- choose [random pop]
@@ -54,16 +57,27 @@
   (let [fuels (map unjava-fuel fuels)]
     (map #(apply map list %) fuels)))
 
-(defn genetic-solve-car-from-string [car-string min-ingred max-ingred pop-size init-max max-mutate max-generations]
-  (let [[rest-string car] (parse-car car-string)]
-    (cond (not (empty? rest-string)) [false "car does not parse"]
-	  (< (car-tanks car) 2) [false "car does not have at least 2 tanks"]
-	  :else
-	  (loop [num-ingred min-ingred]
-	    (if (> num-ingred max-ingred)
-	      [false "no cars found"]
-	      (let [[gen fit-pop] (genetic-fuels car num-ingred pop-size init-max max-mutate max-generations)
-		    [best best-score] (first fit-pop)]
-		(if (> best-score 0)
-		  [true (koeblerify-fuels best)]
-		  (recur (inc num-ingred)))))))))
+(vsc-fn genetic-solve-car-from-string 1 [car-string min-ingred max-ingred pop-size init-max max-mutate max-generations]
+	(try
+	  (let [[rest-string car] (parse-car car-string)]
+	    (cond (not (empty? rest-string)) [false "car does not parse"]
+		  (< (car-tanks car) 2) [false "car does not have at least 2 tanks"]
+		  :else
+		  (loop [num-ingred min-ingred]
+		    (if (> num-ingred max-ingred)
+		      [false "no cars found"]
+		      (let [[gen fit-pop] (genetic-fuels car num-ingred pop-size init-max max-mutate max-generations)
+			    [best best-score] (first fit-pop)]
+			(if (> best-score 0)
+			  [true (koeblerify-fuels best)]
+			  (recur (inc num-ingred))))))))
+	  (catch Exception exc
+	    [false (str exc)])))
+
+(defn genetic-solve-cars [cars min-ingred max-ingred pop-size init-max max-mutate max-generations]
+  (vsc-pmap 1000 (fn [[id string]]
+		   [id (genetic-solve-car-from-string string min-ingred max-ingred pop-size init-max max-mutate max-generations)])
+	    cars))
+
+(defn read-cars-from-file [filename]
+  (map #(clojure.contrib.string/split #"\s+" %) (clojure.contrib.string/split-lines (slurp filename))))
