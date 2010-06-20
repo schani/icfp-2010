f83519230573061aa5ac446a984a96e9fd07e15e
Tobias Deutsch ICFP <toby@ti11.(none)>
2010-06-20 16:45:16 +0200

    added we have shorter fuel msg

--- scripts/submit_new_fuel_against_all.pl
+++ scripts/submit_new_fuel_against_all.pl
@@ -47,7 +47,10 @@ foreach $car (keys %known_cars) {
 	my $carfuel = $known_cars{$car};
 	
 	# don't try if we already have better fuel
-	next if !($carfuel eq "-") && $fuels{$carfuel} < $new_fuel_size; 
+	if ( !($carfuel eq "-") && ($fuels{$carfuel} < $new_fuel_size) ) {
+            print "skiping ${car}, we already have shorter fuel.\n";
+            next;
+        }
 
 	# try newfuel on the car
 	print "./submit.pl fuel $car - <data/fuel_${new_fuel}.txt : ";
