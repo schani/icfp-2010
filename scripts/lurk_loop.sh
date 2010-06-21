3803bff7bec6619c3968fd5e2deeb194b607af32
Tobias Deutsch ICFP <toby@ti11.(none)>
2010-06-21 02:44:07 +0200

    script update

--- scripts/lurk_loop.sh
+++ scripts/lurk_loop.sh
@@ -4,6 +4,9 @@ while true; do
     echo "new lurk/generate loop round started"
     echo "lurking!"
     ./lurk_submit_cars.pl
+    git commit scripts/data/allcars.txt -m autoupdate
+    git commit scripts/data/known_cars.txt -m autoupdate
+
     echo "badcars!"  
     ./generate_badcars.sh
     echo "sleeping"
