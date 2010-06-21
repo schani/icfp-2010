2effdf4e7ed3e9cdef714334d700aa8ae20d68e2
Heinz Deinhart <heinz@nix.at>
2010-06-21 05:34:30 +0200

    keien ahnung

--- submissions/find-1team-cars.sh
+++ submissions/find-1team-cars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cat | awk '/: 1 teams/ {carid=$2} /size/ {if (carid > 0) { printf "carid %i size %i\n", carid, $2; carid=0}}' | sort -nr -k 4
+# egrep "$(echo $(./find-1team-cars.sh < cars | awk '{print $2}' ) | sed 's# # |#g')" ../scripts/data/badcars.txt > hipricars.txt
