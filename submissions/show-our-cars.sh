423fd4c53c3c4884c8d19c6710aa996468a2c177
Heinz Deinhart <heinz@nix.at>
2010-06-21 05:34:21 +0200

    wow

--- submissions/show-our-cars.sh
+++ submissions/show-our-cars.sh
@@ -1,1 +1,1 @@
-egrep -A1 "19145|44907|52926|55796|59036|59708|63268|112112|68100|$(echo $(grep success submittable-cars-2010-06-21-03:04:51.log | awk '{gsub("carid=","");gsub(",","");print $2}') | sed 's# #|#g')" cars
+egrep -A1 "19145|44907|52926|55796|59036|59708|63268|112112|68100|$(echo $(grep success submittable-cars-*.log | awk '{gsub("carid=","");gsub(",","");print $2}') | sed 's# #|#g')" cars | grep -v -- --
