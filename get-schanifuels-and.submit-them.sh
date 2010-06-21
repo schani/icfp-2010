61d01a2f93349196f9399067924adbc05f1ef471
Tobias Deutsch ICFP <toby@ti11.(none)>
2010-06-21 06:20:20 +0200

    schanifuels6

--- get-schanifuels-and.submit-them.sh
+++ get-schanifuels-and.submit-them.sh
@@ -8,7 +8,7 @@
 
 RUNTIME=`date +"%F-%T"`
 
-SCHANIFILE="submissions/genetic-fuels-5"
+SCHANIFILE="submissions/genetic-fuels-6"
 
 SUBMISSIONFILE="submissions/submittable-fuels-$RUNTIME.txt"
 SUBLISSIONLOGFILE="submissions/submittable-fuels-$RUNTIME.log"
